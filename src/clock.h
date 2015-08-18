23bdb10a8991673dfb9e5cfbf6922ee0f68c10cb
Andreas Seuss <aseuss@seussnet.de>
2015-08-18 23:28:02 +0200

    add clock class

---
+++ src/clock.h
@@ -0,0 +1,32 @@
+
+/*
+ * src/clock.h
+ *
+ * Copyright 2014,2015 Andreas Seuss
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+
+#ifndef BLACKELDER_CLOCK_H
+#define BLACKELDER_CLOCK_H
+
+namespace be {
+
+class Clock {
+public:
+    static long getElapsedTime();
+};
+
+} // end of namespace be
+
+#endif // BLACKELDER_CLOCK_H
